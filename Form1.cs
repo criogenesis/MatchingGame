8532a2d0acdd505049c36acadcbc56762fbf40e8
criogenesis <hakkuo23@gmail.com>
2025-05-06 21:44:49 -0400

    Added Function to assign icons randomly to the squares on the grid

--- Form1.cs
+++ Form1.cs
@@ -30,6 +30,28 @@ namespace MatchingGame
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
 
+        /// <summary>
+        /// Assign icons to each square randomly
+        /// </summary>
+        private void AssignIconsToSquare()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if(iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
